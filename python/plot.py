0aaaa6136b01c3acbb75f5f6bcbe89758589aaa5
Arseny Lychev <arsenii.lychev@math.msu.ru>
2026-06-04 00:02:04 +0300

    Поправил

--- python/plot.py
+++ python/plot.py
@@ -3,6 +3,7 @@ from plotters import heat_conduction_reference_example_plotter as hcrep
 from plotters.three_body_plotter import ThreeBodyPlotter
 
 if __name__ == '__main__':
+
     if len(sys.argv) < 4:
         print('Usage: plot.py <plotter> <json_data_path> <output_path>')
         raise SystemError
@@ -13,9 +14,11 @@ if __name__ == '__main__':
         "ThreeBodyPlotter": ThreeBodyPlotter,
     }
 
-    if sys.argv[1] not in plotters:
+    if not (sys.argv[1] in plotters):
         raise SystemError
 
     Plotter = plotters[sys.argv[1]]
+
     plotter = Plotter(sys.argv[2], sys.argv[3])
+
     plotter.plot()
